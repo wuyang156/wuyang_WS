f263de9c1a31aa3e16e59722de460d712e0b4770
无氧 <1481435954@qq.com>
2026-05-06 23:48:17 +0800

    第十八次提交,BUG:/joint_state话题为空

---
+++ src/wuyang_description/launch/gazebo.launch.py
@@ -0,0 +1,74 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.event_handlers import OnProcessExit
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    pkg_share = FindPackageShare('wuyang_description')
+    gazebo_ros_share = FindPackageShare('gazebo_ros')
+    xacro_file = PathJoinSubstitution([pkg_share, 'urdf', 'wuyang_ackermann.urdf.xacro'])
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': ParameterValue(Command(['xacro ', xacro_file]), value_type=str),
+            'use_sim_time': True
+        }]
+    )
+ 
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([gazebo_ros_share, 'launch', 'gazebo.launch.py'])
+        )
+    )
+ 
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', '/robot_description', '-entity', 'wuyang_car', '-z', '0.05'],
+        output='screen'
+    )
+
+    load_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    load_ackermann_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ackermann_controller"],
+    )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        gazebo,
+        spawn_entity,
+        rviz2_node,
+        
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_ackermann_controller],
+            )
+        ),
+    ])
